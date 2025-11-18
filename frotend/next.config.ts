ddf8b7b480f4347711eb5e545fe3b9e4dc0f7fef
webtrixztech <hamidaliwebtrixz@gmail.com>
2025-11-18 12:56:46 +0530

    first commit

--- frotend/next.config.ts
+++ frotend/next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
    env: {
     HOSTNAME: "localhost",
-    APIBASEURL: "http://localhost:8080",
+    APIBASEURL: "https://real-stock-price.onrender.com",
   },
 };
 
